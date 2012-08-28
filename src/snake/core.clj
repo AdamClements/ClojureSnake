bdaf13c1c9858290e56c9d5a7ca11041da377387
Adam Clements <adam.clements@gmail.com>
2012-08-28 23:58:46 +0100

    Split drawing from game logic and made everything a mite more functional with decorator patterns. Possibly overkill.

--- src/snake/core.clj
+++ src/snake/core.clj
@@ -1,13 +1,11 @@
 (ns snake.core
   (:require [quil.core  :refer :all]
             [clojure.set :refer [difference
+                                 intersection
                                  union]]))
 
 (def ^:dynamic *playing-field* [20 20])
-(def default-snake '((10 10) (9 10) (8 10)))
-(def food     (atom '(2 2)))
-(def my-snake (atom default-snake
-     :validator #(= (count %) (count (distinct %)))))
+
 
 (defn next-dest [[head trail & _]]   (map - head trail))
 (defn wrap-around-world [dest]       (map mod dest *playing-field*))
@@ -19,8 +17,7 @@
          :left  [-1  0]
          :right [ 1  0]
          (next-dest snake))
-       (map + (first snake))
-       (wrap-around-world)))
+       (map + (first snake))))
 
 (defn random-empty-pt [oldfood filled]
   (let [[max-x max-y] *playing-field*]
@@ -29,68 +26,54 @@
         (difference (set oldfood) (set filled))
         (seq) (rand-nth))))
 
-(defn eat [desti snake]
-  (swap! food random-empty-pt snake)
-  (frame-rate (inc (current-frame-rate)))
-  (cons desti snake))
+(defn default-snake []
+  (let [start-pt (map / *playing-field* (repeat 2))]
+    (map (partial map + start-pt) [[0 0] [0 1] [0 2]])))
 
-(defn move [snake direction]
-  (let [desti (dest snake direction)]
-    (if (= desti @food)
-      (eat desti snake)
-      (cons desti (butlast snake)))))
+(defn default-world []
+  {:snake      (default-snake)
+   :speed      1
+   :food       (random-empty-pt nil (default-snake))
+   :game-over? false})
 
-(defn draw-food [[x y]]
-  (stroke 0 198 34)
-  (rect-mode :center)
-  (rect x y 0 0))
+(defn eat-self [app]
+  (fn [{snake :snake :as world}]
+    (app (if (apply distinct? snake) world
+           (assoc world :game-over? true)))))
 
-(defn connected? [[a b]]
-  (= 1 (abs (reduce + (map - a b)))))
+(defn eat-food [app]
+  (fn [{:keys [snake food speed] :as world}]
+    (app (if (empty? (intersection (set snake) #{food}))
+           (assoc world :snake (butlast snake))
+           (assoc world :food  (random-empty-pt #{food} snake)
+                        :speed (inc speed))))))
 
-(defn connect-lines [object]
-  (apply concat (filter connected? (partition 2 1 object))))
+(defn wrap [app]
+  (fn [{snake :snake :as world}]
+    (app (assoc world :snake (map wrap-around-world snake)))))
 
-(defn draw-snake [snake]
-  (stroke 10 20 10)
-  (begin-shape :lines)
-  (doall (map #(apply vertex %) (connect-lines snake)))
-  (end-shape))
+(defn move [app]
+  (fn [{:keys [events snake] :as world}]
+    (let [[event & other] events
+          moved-snake     (cons (dest snake event) snake)]
+      (app (assoc world :events other :snake moved-snake)))))
 
-(defn draw-background []
-  (stroke 30 50 30)
-  (fill 50 80 50)
-  (rect-mode :corner)
-  (apply rect -1/2 -1/2 *playing-field*))
+(defn restart-game [app]
+  (fn [{:keys [events] :as world}]
+    (if (or (nil? world) (= (first events) :f2))
+      (default-world)
+      (app world))))
 
-(defn draw-gameover []
-  (fill 200 30 30 200)
-  (text-mode :model)
-  (text-size 2)
-  (text-align :center :center)
-  (text "Game over\nF2?" 9.5 9.5))
+(defn game-running [app]
+  (fn [{:keys [game-over? events] :as world}]
+    (if game-over?
+      world
+      (app world))))
 
-(defn to-screen []
-  (apply scale (map / (repeat 2 (min (width) (height))) *playing-field*))
-  (translate 0.5 0.5))
-
-(defn new-game! []
-  (reset! my-snake default-snake)
-  (frame-rate 3))
-
-(defn draw []
-  (background 0)
-  (when (= :f2 (key-as-keyword)) (new-game!))
-
-  (to-screen)
-  (draw-background)
-  (draw-snake @my-snake)
-  (draw-food  @food)
-
-  (try (swap! my-snake move (key-as-keyword))
-  (catch IllegalStateException _
-    (draw-gameover))))
-
-(defsketch snake-game :title "SNAKES"
-                      :setup new-game!
-                      :draw  draw)
+(def game (-> identity
+           eat-self
+           eat-food
+           wrap
+           move
+           game-running
+           restart-game))
--- src/snake/drawquil.clj
+++ src/snake/drawquil.clj
@@ -2,18 +2,61 @@
   (:require [quil.core :refer :all]
             [snake.core :refer :all]))
 
-(def ^:dynamic *playing-field* [50 50])
+(def world (atom nil))
 
-(defn draw-block 
-  [x y]
-  (let [[w h] (map / [(width) (height)] *playing-field*)]
-    (rect (* x w) (* y h) w h)))
+(defn connected? [[a b]]
+  (= 1 (abs (reduce + (map - a b)))))
 
-(defn draw-object
-  [object]
-  (doall (map draw-block object)))
+(defn connect-lines [object]
+  (apply concat (filter connected? (partition 2 1 object))))
+
+(defn draw-snake [snake]
+  (stroke 10 20 10)
+  (begin-shape :lines)
+  (doall (map #(apply vertex %) (connect-lines snake)))
+  (end-shape))
+
+(defn draw-food [[x y]]
+  (stroke 0 198 34)
+  (rect-mode :center)
+  (rect x y 0 0))
+
+(defn draw-background []
+  (stroke 30 50 30)
+  (fill 50 80 50)
+  (rect-mode :corner)
+  (apply rect -1/2 -1/2 *playing-field*))
+
+(defn draw-gameover []
+  (fill 200 30 30 200)
+  (text-mode :model)
+  (text-size 2)
+  (text-align :center :center)
+  (text "Game over\nF2?" 9.5 9.5))
+
+(defn to-screen []
+  (apply scale (map / (repeat 2 (min (width) (height))) *playing-field*))
+  (translate 0.5 0.5))
+
+(defn stack-events [& args]
+  (let [new-key (key-as-keyword)
+        {events :events} @world]
+    (when (and (#{:f2 :up :down :left :right} new-key)
+               (not= (first events) new-key))
+      (swap! world assoc :events (cons new-key events)))))
+
+(defn draw []
+  (let [{:keys [game-over? snake food events speed]} (swap! world game)]
+    (frame-rate (or speed 1))
+    (background 0)
+    (to-screen)
+    (draw-background)
+    (draw-snake snake)
+    (draw-food food)
+    (when game-over? (draw-gameover))))
 
 (defsketch snake-game
-           :title "SNAKES"
-           :draw  draw
-           :setup (fn [] (frame-rate 2)))
+  :title "SNAKES"
+  :key-pressed stack-events
+  :setup #(frame-rate 1)
+  :draw  draw)
